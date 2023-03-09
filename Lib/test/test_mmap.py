88895755fb6b92adf0c1a4ef8ef131525a53c97d
Jeong YunWon <jeong@youknowone.org>
2023-03-09 22:43:15 +0900

    Fix sequence protocol concat

--- Lib/test/test_mmap.py
+++ Lib/test/test_mmap.py
@@ -721,8 +721,6 @@ class MmapTests(unittest.TestCase):
         self.assertRaises(ValueError, m.write_byte, 42)
         self.assertRaises(ValueError, m.write, b'abc')
 
-    # TODO: RUSTPYTHON
-    @unittest.skip
     def test_concat_repeat_exception(self):
         m = mmap.mmap(-1, 16)
         with self.assertRaises(TypeError):
